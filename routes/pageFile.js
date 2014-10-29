bfffb4ce9f09ff73d794a82f491b20e9d7b88ed0
coolfish <461836324@qq.com>
2014-10-29 19:49:52 +0800

    update

--- routes/pageFile.js
+++ routes/pageFile.js
@@ -5,12 +5,15 @@ var async = require('async'),
 //imgSaveToFile
 exports.imgSaveToFile = function(req, res){
 	var path = '';
+	console.log('[0]',req.files.file);
 	async.series({
 		getUrl:function(done){
+			console.log('[1]:',!/image/.test(req.files.mimetype),req.files.file.mimetype);
 			if(!/image/.test(req.files.file.mimetype)){
 				path = req.files.file.path;
 				done('上传的不是图片格式，请检查');
 			}else{
+				console.log('[img]:',req.files.file);
 				path = req.files.file.path;
 				path = path.replace(config.FILEPATH,'');
 				path = path.replace('public/','');
@@ -18,6 +21,7 @@ exports.imgSaveToFile = function(req, res){
 			}
 		}
 	},function(err){
+		console.log('--',err,path);
 		if(err){
 			fs.unlink(path);
             res.send({status: -1, content: err});
