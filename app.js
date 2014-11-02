4fde0bb4387413dcb353cf55b1f1ba8f73d62a21
coolfish <461836324@qq.com>
2014-11-03 00:00:48 +0800

    add 404

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ app.use('/', routes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    // var err = new Error('Not Found');
+    // err.status = 404;
+    // next(err);
+    res.render("404");
 });
 
 /// error handlers
