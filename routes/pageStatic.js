494a8604715852f0d900a17d608cf922191aa51d
coolfish <461836324@qq.com>
2015-01-18 23:20:45 +0800

    update

--- routes/pageStatic.js
+++ routes/pageStatic.js
@@ -7,80 +7,81 @@ var user = require('./module/user'),
 
 //跳转到首页页面
 exports.gotoIndex = function(req, res){
-	var userInfo = '';
-	if(!req.session || !req.session.user){
-		userInfo = '';
-	}else{
-		userInfo = req.session.user;	
-	}
-	var page = req.query.page || 1;
-    var type = req.query.type || 'all';
-	var topicList = [];
-    var count = 0;
-    var info = {};
-    var userIntegral = 0;
-    if('jp' == type){
-        info.isGood = true;
-    }else if('all' != type){
-        info.tag = type;
-    }
-    info.isOpen = true;
-	async.series({
-    	//获取列表信息
-        findTopicList: function(done){
-        	topic.getAll(config.LIMIT.INDEXPAGENUM, page, info, function(err, info){
-        		for(var i = 0; i < info.length; i++){
-        			info[i].lastTime = tool.getDateDiff(info[i].lastTimestamp);
-        		}
-        		topicList = info;
-        		done(err);
-        	});
-        },
-        //获取对应的用户信息
-        findUserInfo: function(done){
-            var iterator = function(topicInfo, eachFinish){
-                user.getById(topicInfo.userId, function(err, dbUserInfo){
-                    topicInfo.userName = dbUserInfo.name;
-                    topicInfo.userHeadSrc = dbUserInfo.headSrc;
-                    user.getById(topicInfo.lastUser, function(err, dbLastUserInfo){
-                        topicInfo.lastUserName = dbLastUserInfo.name;
-                        topicInfo.lastUserHeadSrc = dbLastUserInfo.headSrc;
-                        eachFinish();
-                    });   
-                });
-            };
-            async.forEach(topicList, iterator, function(err){
-                done(err);
-            });
-        },
-        //获取总数
-        findCount : function(done){
-            info.isWaste = false;
-            info.isOpen = true;
-            topic.getCount(info, function(err, info){
-                count = Math.ceil(info/config.LIMIT.INDEXPAGENUM);
-                done(err);
-            });
-        },
-        findIntegral : function(done){
-            if(!req.session || !req.session.user){
-                done();
-            }else{
-                integral.getById(userInfo._id,function(err, info){
-                    userIntegral = info.integral || 0;
-                    done(err);
-                });
-            }
-        },
-        findTopicCount : function(done){
-            topic.getCount({userId : userInfo._id}, function(err, info){
-                userInfo.topicCount = info;
-                done(err);
-            });
-        }
-    }, function(err){
-        res.render('effect', { titleName: config.NAME ,user : userInfo, topicList : topicList, count : count, currentPage : page, currentType : type, userIntegral : userIntegral});  
-    })
+	// var userInfo = '';
+	// if(!req.session || !req.session.user){
+	// 	userInfo = '';
+	// }else{
+	// 	userInfo = req.session.user;	
+	// }
+	// var page = req.query.page || 1;
+ //    var type = req.query.type || 'all';
+	// var topicList = [];
+ //    var count = 0;
+ //    var info = {};
+ //    var userIntegral = 0;
+ //    if('jp' == type){
+ //        info.isGood = true;
+ //    }else if('all' != type){
+ //        info.tag = type;
+ //    }
+ //    info.isOpen = true;
+	// async.series({
+ //    	//获取列表信息
+ //        findTopicList: function(done){
+ //        	topic.getAll(config.LIMIT.INDEXPAGENUM, page, info, function(err, info){
+ //        		for(var i = 0; i < info.length; i++){
+ //        			info[i].lastTime = tool.getDateDiff(info[i].lastTimestamp);
+ //        		}
+ //        		topicList = info;
+ //        		done(err);
+ //        	});
+ //        },
+ //        //获取对应的用户信息
+ //        findUserInfo: function(done){
+ //            var iterator = function(topicInfo, eachFinish){
+ //                user.getById(topicInfo.userId, function(err, dbUserInfo){
+ //                    topicInfo.userName = dbUserInfo.name;
+ //                    topicInfo.userHeadSrc = dbUserInfo.headSrc;
+ //                    user.getById(topicInfo.lastUser, function(err, dbLastUserInfo){
+ //                        topicInfo.lastUserName = dbLastUserInfo.name;
+ //                        topicInfo.lastUserHeadSrc = dbLastUserInfo.headSrc;
+ //                        eachFinish();
+ //                    });   
+ //                });
+ //            };
+ //            async.forEach(topicList, iterator, function(err){
+ //                done(err);
+ //            });
+ //        },
+ //        //获取总数
+ //        findCount : function(done){
+ //            info.isWaste = false;
+ //            info.isOpen = true;
+ //            topic.getCount(info, function(err, info){
+ //                count = Math.ceil(info/config.LIMIT.INDEXPAGENUM);
+ //                done(err);
+ //            });
+ //        },
+ //        findIntegral : function(done){
+ //            if(!req.session || !req.session.user){
+ //                done();
+ //            }else{
+ //                integral.getById(userInfo._id,function(err, info){
+ //                    userIntegral = info.integral || 0;
+ //                    done(err);
+ //                });
+ //            }
+ //        },
+ //        findTopicCount : function(done){
+ //            topic.getCount({userId : userInfo._id}, function(err, info){
+ //                userInfo.topicCount = info;
+ //                done(err);
+ //            });
+ //        }
+ //    }, function(err){
+        // res.render('effect', { titleName: config.NAME ,user : userInfo, topicList : topicList, count : count, currentPage : page, currentType : type, userIntegral : userIntegral});  
+    // })
+    res.render('effect', { });
 };
 
 //跳转到静态工具页面
